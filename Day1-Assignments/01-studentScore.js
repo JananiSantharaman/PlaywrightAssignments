8f0fe06a9eb58b5b7e8ce9ac10d003d532703833
jananisantharaman@gmail.com <jananisantharaman@gmail.com>
2023-11-26 19:17:03 +1100

    file3

---
+++ Day1-Assignments/01-studentScore.js
@@ -0,0 +1,42 @@
+
+/*
+1. Create a function named `calculateGrade` that takes a student's score as a parameter.
+
+2. Use `switch` statement inside the function - 
+   - Use a `switch` statement with the condition `true`.
+   - Use `case` statements to check different score ranges and return the corresponding grade.
+
+3. Declare and initialize the variable
+
+4. Call the function and print the result
+
+//grading input
+/60–100	A
+50–59	B
+40–49	C
+<40	F
+*/
+const studentScore = 100
+var grade = calculateGrade(studentScore)
+console.log(grade)
+//function
+function calculateGrade(score) {
+    if ((typeof score === "number") && (score <= 100)){
+    switch (true) {
+        case (score >= 60 && score <= 100):
+            return "Student grade is - A"
+            break;
+        case (score >= 50 && score <= 59):
+            return "Student grade is - B"
+            break;
+        case (score >= 40 && score <= 49):
+            return "Student grade is - C"
+            break;
+        default: (score < 40)
+            return "Student grade is - D"
+            break;
+    }
+}else{
+    return "Please enter a valid score"
+}      
+}
